06988bff823eb1b38ee4933ae5ec2546b6ee504d
Camden Meyer <106187107+Meyer-C@users.noreply.github.com>
2022-11-15 18:22:18 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from PIL import Image
 import itertools
 
-combo_num = 2
-layer_num = 2
-gen_dir = input(r'Enter Filepath to layers to combine: ')
+def get_paths():
+    with open(r'C:\Users\Camden\PycharmProject\nft_layering\paths.txt', 'r') as a:
+        b = a.readlines()
+        project_name = ((b[0].split('=')[1]).replace(' ', '')).replace('\n', '')
+        in_gen_path = ((b[1].split('=')[1]).replace(' ', '')).replace('\n', '')
+        out_path = ((b[2].split('=')[1]).replace(' ', '')).replace('\n', '')
+        layers = int(((b[3].split('=')[1]).replace(' ', '')).replace('\n', ''))
+        combos = int(((b[4].split('=')[1]).replace(' ', '')).replace('\n', ''))
 
-final_paths = []
-for a in range(layer_num):
-    layer_paths = []
-    layer_path = gen_dir + '\layer_' + str(a+1)
-    for b in range(combo_num):
-        layer_paths.append(layer_path + '\layer_' + str(a+1) + '_' + str(b+1) + '.png')
-    final_paths.append(layer_paths)
+        input_strs = []
+        for x in range(layers):
+            layer_strs = []
+            for y in range(combos):
+                layer_strs.append(str(f'{in_gen_path}\\layer_{x+1}\\layer_{x+1}_{y+1}.png'))
+            input_strs.append(layer_strs)
 
-combos = (list(itertools.product(range(combo_num), range(combo_num))))
+    return (input_strs, project_name, out_path)
 
+def layer(paths_data, show=False, save=False):
+    input_strs = paths_data[0]
+    project_name = paths_data[1]
+    out_path = paths_data[2]
+    combos = list(itertools.product(*input_strs))
+    for x, combo in enumerate(combos):
+        image = Image.open(combo[0])
+        out_filepath = str(f'{out_path}\\{project_name}_{x}.png')
+        for path in combo[1::]:
+            overlay = Image.open(path)
+            image.paste(overlay, (0, 0), overlay)
+        if show == True and save == True:
+            image.show()
+            image.save(out_filepath)
+        elif show == True:
+            image.show()
+        elif save == True:
+            image.save(out_filepath)
 
-for x in range(len(combos)):
-    # create all combo file path lists
-    combo = list(combos[x])
-    path_combos = []
-    for y in range(len(combo)):
-        part_num = combo[y]
-        path_list = final_paths[y]
-        path_combos.append(path_list[part_num])
-
-    final = Image.open(path_combos[0])
-    for z in range(len(path_combos)):
-        if z >> 0:
-            overlay = Image.open(path_combos[z])
-            final.paste(overlay,(0,0),overlay)
-    final.show()
-
-
-
-
-
+layer(get_paths(), show=True, save=True)
